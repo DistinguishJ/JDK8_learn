eca6c3314e62482b3d79f0a2677eddfdfa5fbad5
Jin <jinhy549@163.com>
2021-03-27 17:23:45 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        System.out.println("12");
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(12,"12");
+        map.put(34,"34");
+        System.out.println(map);
+    }
+
+}
